d1377584d6c6ac9bc8e9d97d1dc16ad9f2aa7a9a
pinakiweb <psmailpinaki@gmail.com>
2020-11-21 00:07:05 -0700

    small commit in Phase3.py

--- Phase3.py
+++ Phase3.py
@@ -1,12 +1,13 @@
 import task1
 import task2_knn
-
+import task3
 def main():
     while True:
         print("########## Phase 3 ##########")
         print("Task 1: Personalized Pagerank")
         print("Task 2a: KNN classification")
         print("Task 2b: PPR classification")
+        print("Task 3: Find similar gestures using LSH")
         task = input("What Task do you want to perform: (enter 0 to exit)\n")
         # folder_directory = input("Input Directory path of the gesture folders:\n")
         if task == '1':
@@ -21,6 +22,10 @@ def main():
             print("########## Task 2 ##########")
             task2_ppr.main()
             print("########## Task 2b Completed ##########\n")
+        elif task == '3':
+            print("########## Task 3 ##########")
+            task3.main()
+            print("########## Task 3 Completed ##########\n")
         elif task == '0':
             exit()
 
